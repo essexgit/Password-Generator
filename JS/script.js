142b1cc7c083015927b8ed1a4e521826a662d577
Ralph Cox <73188137+essexgit@users.noreply.github.com>
2023-01-06 13:47:16 +0000

    first function froamwork in place

--- JS/script.js
+++ JS/script.js
@@ -88,31 +88,78 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// Function to prompt user for password options
-function getPasswordOptions() {
-
-}
-
-// Function for getting a random element from an array
-function getRandom(arr) {
-
-}
+//Pseudo code
+// Displays a password
 
 // Function to generate password with user input
 function generatePassword() {
+  console.log('Button works');
+}
+// Asks for length of password
+
+// Asks user for password criteria
+function getPasswordOptions() {
 
 }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
-
-  passwordText.value = password;
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+// Write password to the #password input
+function writePassword() {
+
+  var password = generatePassword();
+
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = "test";
+  // passwordText.value = password;
+}
+
+// PWOptions object
+
+let PWOptions = {
+  length: 0,
+  numbers: false,
+  special: false,
+  lower: false,
+  upper: false
+};
+
+// Function to prompt user for password options
+// check if 5 options chosen
+// Create an array of length set by user
+prompt('Enter a password length between 10 and 64 charachters');
+
+// Asks if numbers are to be included
+function confirmNumbers() {
+  if (confirm('Do you want numbers (3,7,6) in your password?')) {
+    PWOptions.numbers = true;
+  }
+}
+
+// Asks if special characters are to be included
+
+function confirmSpecial{
+  if (confirm('Do you want special characters ({,],/) in your password?')
+  ) {
+    PWOptions.special = true;
+  }
+}
+
+// Asks if lowercase characters are to be included
+(confirm('Do you want lowercase characters (w,f,g) in your password?')
+)
+  // Asks if uppercase characters are to be included
+  (confirm('Do you want uppercase characters (S,T,P) in your password?')
+  );
+// Populate array with random characters selected from character array
+let characterArray;
+// Create character array from selections by user
+// Function for getting a random element from an array
+function getRandom(arr) {
+
+}
